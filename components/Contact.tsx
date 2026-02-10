8abd43b1947b11ce915922794818d1249f87e335
Alex <alex@greenshift.local>
2026-02-10 11:05:00 +0100

    Updated Facebook link to actual page URL

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -31,7 +31,7 @@ const Contact: React.FC<ContactProps> = ({ onShowAbout, onSubscribeClick }) => {
               <Instagram size={14} />
             </a>
             <a
-              href="https://facebook.com/planetarybrief"
+              href="https://www.facebook.com/profile.php?id=61587322058668"
               target="_blank"
               rel="noopener noreferrer"
               className="w-8 h-8 rounded-full border border-white/10 hover:border-white/30 flex items-center justify-center text-gray-500 hover:text-white transition-all duration-300 hover:scale-110"
